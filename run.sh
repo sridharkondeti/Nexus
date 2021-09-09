fc649e8f730bf7959d71700fef28cd9f56383f19
sridharkondeti <sridharreddy2kx@gmail.com>
2021-09-08 23:52:45 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 kubectl apply -f deployment.yaml
 
 kubectl apply -f service.yaml
+
+# kubectl commands to create deployments and services
+# with out yaml files.
+
+kubectl create deployment nexus --image=sonatype/nexus3:latest
+
+kubectl expose deployments nexus --port=80 --type=LoadBalancer
+
+kubectl get svc
+
+kubectl get pods
+
+kubectl get deployments
